39c285ac1fc12684bab5c3566efcc443a8a67ffc
apkiys <apkiys@gmail.com>
2023-07-02 01:07:46 +0800

    Create Instances

--- main.tf
+++ main.tf
@@ -1,12 +1,10 @@
 provider "aws" {
-  region = "ap-northeast-3"
-  # Edit below
+  region = "ap-southeast-1"
   }
 
 resource "aws_instance" "instances" {
   ami           = "ami-013bdaf9892a21e7e"
   instance_type = "t2.micro"
-  key_name = "Webserver_Keypair"
   count         = 2
 
   tags = {
