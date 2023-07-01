3ba5123e4b4bb356d4dfe496a1548a8de14a69be
apkiys <apkiys@gmail.com>
2023-07-01 22:42:10 +0800

    Removing sensitive data

--- main.tf
+++ main.tf
@@ -7,7 +7,6 @@ resource "aws_instance" "instances" {
   ami           = "ami-013bdaf9892a21e7e"
   instance_type = "t2.micro"
   key_name = "Webserver_Keypair"
-  availability_zone = "${AWS::Region}a"
   count         = 2
 
   tags = {
