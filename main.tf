738b87f9189321066456406126dde46a1cafa787
apkiys <apkiys@gmail.com>
2023-07-01 22:36:45 +0800

    Removing sensitive data

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 provider "aws" {
   region = "ap-northeast-3"
   # Edit below
-  access_key = "AKIATXF4JQPH72SRLNHF"
-  secret_key = "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"
   }
 
 resource "aws_instance" "instances" {
-  ami           = "ami-013bdaf9892a21e7e"  # Amazon Linux 2 LTS
+  ami           = "ami-013bdaf9892a21e7e"
   instance_type = "t2.micro"
+  key_name = "Webserver_Keypair"
+  availability_zone = "${AWS::Region}a"
   count         = 2
 
   tags = {
