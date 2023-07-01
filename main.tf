ab904b53e840655e6126a77b09519aea7a75cc83
apkiys <apkiys@gmail.com>
2023-07-02 01:41:10 +0800

    Create Instances 4

--- main.tf
+++ main.tf
@@ -3,9 +3,9 @@ provider "aws" {
   }
 
 resource "aws_instance" "instances" {
-  ami           = "ami-013bdaf9892a21e7e"
+  ami           = "ami-08928044842b396f0"
   instance_type = "t2.micro"
-  count         = 2
+  count         = 1
 
   tags = {
     Name = "Webserver-${count.index + 1}-Test"
